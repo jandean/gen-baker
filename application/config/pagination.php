6ebb10143633d80ed2eddf0a6955c380ed8faa8d
Jan-Dean Fajardo <jdfajardo@sulit.com.ph>
2014-04-30 22:23:13 +0800

    Some changes for server

--- application/config/pagination.php
+++ application/config/pagination.php
@@ -1,4 +1,4 @@
- <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['per_page'] = 20;
 
